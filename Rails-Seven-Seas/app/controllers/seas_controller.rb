402f8bab4279af7a3aeadb751e9faa40a3a67b6e
github username <github email address>
2018-11-02 16:09:28 -0400

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,43 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  def index
+    @seas = Sea.all
+  end
 
+
+  def show
+    @sea =Sea.find(params[:id])
+  end
+
+  def welcome
+
+  end
+
+  def new
+    @sea = Sea.create
+  end
+
+  def create
+    @sea = Sea.create(params[:sea])
+    @sea.update(sea_params)
+    redirect_to "/seas/#{@sea.id}"
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+    redirect_to "/seas/#{@sea.id}"
+  end
+
+  def destroy
+    @sea = Sea.find(params[:id])
+    @sea.delete
+    redirect_to "/seas"
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
@@ -9,5 +46,6 @@ class SeasController < ApplicationController
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
+
 
 end
